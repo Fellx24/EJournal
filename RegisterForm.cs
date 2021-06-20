1e2ec0d4fb9d7edfe374c134b7c303c6f6801784
Fellx24 <gavrilov31072003@gmail.com>
2021-06-20 22:38:23 +0700

    register

--- RegisterForm.cs
+++ RegisterForm.cs
@@ -1,5 +1,6 @@
 ﻿using Npgsql;
 using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace EJournal
@@ -24,13 +25,24 @@ namespace EJournal
         {
             string insertquery = $"INSERT INTO public.teacher(surname,name,fathername,email) VALUES ({SurnameTB.Text},{NameTB.Text},{FathernameTB.Text},\'{EmailTB.Text}\')";
             conn.Open();
+
             NpgsqlDataReader reader = null;
             NpgsqlCommand emailcheck = new NpgsqlCommand($"SELECT email FROM public.teacher", conn);
             reader = emailcheck.ExecuteReader();
+            string email = " ";
+            foreach (var item in reader)
+            {
+                email += reader["email"].ToString() + " ";
+            }
+            string[] emailmas = email.Split(' ').ToArray();
             conn.Close();
-                try
-                {
-                    
+            if (emailmas.Contains(EmailTB.Text))
+            {
+                MessageBox.Show("E-mail уже зарегестрирован");
+                conn.Close();
+            }
+            else if (EmailTB.Text.Contains('@'))
+            {
                 conn.Open();
                 NpgsqlCommand insertinfo = new NpgsqlCommand(insertquery, conn);
                 insertinfo.ExecuteReader();
@@ -41,19 +53,12 @@ namespace EJournal
                 conn.Close();
                 MenuForm menu = new MenuForm();
                 menu.Show();
-                }
-                catch (NpgsqlException)
-                {
-                    MessageBox.Show("Неправильный ввод либо E-mail уже зарегестрирован");
-                    conn.Close();
-                }
-
+            }
             conn.Close();
         }
 
         private void RegisterLabel_Click(object sender, EventArgs e)
         {
-            
         }
     }
 }
